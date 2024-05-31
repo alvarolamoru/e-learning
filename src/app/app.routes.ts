e184ddf90cff47ad84ddabde9394034df7b3bce5
Alvaro Lamoru Lopez <alvarolamoru@gmail.com>
2024-05-31 16:09:06 -0400

    creando los componentes referentes en el navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { CursosComponent } from './components/cursos/cursos.component';
 import { AppComponent } from './app.component';
 import { ToolsComponent } from './components/tools/tools.component';
 import { HomeComponent } from './components/home/home.component';
+import { SupportComponent } from './components/support/support.component';
 
 export const routes: Routes = [
    {path:'',component:HomeComponent},
    {path:'tools',component:ToolsComponent},
    {path:'cursos',component:CursosComponent},
+   {path:'support',component:SupportComponent},
 ];
