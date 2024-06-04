4c26dcce68070dde0620eb57cfcec894197897f5
K4L1NUX <114360004+DarkHater01@users.noreply.github.com>
2024-06-04 01:34:15 -0400

    esto esta dp

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,9 +7,15 @@ import { SupportComponent } from './components/support/support.component';
 import { DemoELearningComponent } from './components/demo-e-learning/demo-e-learning.component';
 
 export const routes: Routes = [
+<<<<<<< HEAD
+   {path:'',component:AppComponent},
+   {path:'cursos',component:CursosComponent},
+
+=======
    {path:'',component:HomeComponent},
    {path:'tools',component:ToolsComponent},
    {path:'cursos',component:CursosComponent},
    {path:'support',component:SupportComponent},
    {path:'demo',component:DemoELearningComponent},
+>>>>>>> 1a0487e14ba03b90ffe1cbe7e515ec9048004a64
 ];
